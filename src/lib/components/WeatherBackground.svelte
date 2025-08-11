b5309ec8260fb397af56f568168326c4427edcef
nexryai <nexryai@sda1.net>
2025-08-11 19:21:40 +0900

    Tweak: Update UI

--- src/lib/components/WeatherBackground.svelte
+++ src/lib/components/WeatherBackground.svelte
@@ -26,8 +26,8 @@
         if ([0, 1].includes(code)) {
             useLightText = true;
             bgClass = isDay
-                ? "rounded-3xl w-full h-full bg-[url('https://images.unsplash.com/photo-1597200381847-30ec200eeb9a?q=80&w=2274&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"
-                : "rounded-3xl w-full h-full bg-[url('https://images.unsplash.com/photo-1442876906995-6761040d1f0b?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]";
+                ? "bg-[url('https://images.unsplash.com/photo-1597200381847-30ec200eeb9a?q=80&w=2274&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"
+                : "bg-[url('https://images.unsplash.com/photo-1442876906995-6761040d1f0b?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]";
 
             return;
         }
@@ -35,8 +35,8 @@
         // Partly Cloudy
         if (code === 2) {
             bgClass = isDay
-                ? "rounded-3xl w-full h-full bg-[url('https://images.unsplash.com/photo-1597200381847-30ec200eeb9a?q=80&w=2274&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] "
-                : "rounded-3xl w-full h-full bg-[url('https://images.unsplash.com/photo-1561394104-0904bad68355?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] ";
+                ? "bg-[url('https://images.unsplash.com/photo-1597200381847-30ec200eeb9a?q=80&w=2274&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] "
+                : "bg-[url('https://images.unsplash.com/photo-1561394104-0904bad68355?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] ";
 
             return;
         }
@@ -44,45 +44,47 @@
         // Cloudy
         if (code === 3) {
             bgClass = isDay
-                ? "rounded-3xl w-full h-full bg-[url('https://images.unsplash.com/photo-1534060291226-e30e7693f7ea?q=80&w=2232&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] contrast-50 brightness-125 "
-                : "rounded-3xl w-full h-full bg-[url('https://images.unsplash.com/photo-1561394104-0904bad68355?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] ";
+                ? "bg-[url('https://images.unsplash.com/photo-1534060291226-e30e7693f7ea?q=80&w=2232&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"
+                : "bg-[url('https://images.unsplash.com/photo-1561394104-0904bad68355?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] ";
 
             return;
         }
 
         if (!isDay) {
             // これ以降の夜間の天候の背景は統一
-            bgClass = "rounded-3xl w-full h-full bg-[url('https://images.unsplash.com/photo-1500740516770-92bd004b996e?q=80&w=2344&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] contrast-90 brightness-60 ";
+            bgClass = "bg-[url('https://images.unsplash.com/photo-1500740516770-92bd004b996e?q=80&w=2344&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]";
             return;
         }
 
         // Light rain
         if ([45, 48, 51, 53, 56, 57, 61, 71].includes(code)) {
-            bgClass = "rounded-3xl w-full h-full bg-[url('https://images.unsplash.com/photo-1483702721041-b23de737a886?q=80&w=2346&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] ";
+            bgClass = "bg-[url('https://images.unsplash.com/photo-1483702721041-b23de737a886?q=80&w=2346&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] ";
             return;
         }
 
         // Rain
         if ([55, 63, 65, 66, 67, 73, 75, 77].includes(code)) {
             useLightText = true;
-            bgClass = "rounded-3xl w-full h-full bg-[url('https://images.unsplash.com/photo-1649248230734-bb960c266811?q=80&w=2274&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] ";
+            bgClass = "bg-[url('https://images.unsplash.com/photo-1649248230734-bb960c266811?q=80&w=2274&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] ";
             return;
         }
 
         // Stormy
         if ([82, 95, 96, 99].includes(code)) {
-            bgClass = "rounded-3xl w-full h-full bg-[url('https://images.unsplash.com/photo-1470432581262-e7880e8fe79a?q=80&w=2344&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]";
+            bgClass = "bg-[url('https://images.unsplash.com/photo-1470432581262-e7880e8fe79a?q=80&w=2344&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]";
             return;
         }
 
         // Fallback
-        bgClass = "rounded-3xl w-full h-full bg-[url('https://images.unsplash.com/photo-1523882505971-5c29b55b695c?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] contrast-50 brightness-125 ";
+        bgClass = "bg-[url('https://images.unsplash.com/photo-1523882505971-5c29b55b695c?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]";
     });
 </script>
 
 {#key bgClass}
-    <div in:fade={{ duration: 300 }} id="bg" class={bgClass}>
-        {@render children()}
+    <div in:fade={{ duration: 300 }} id="bg" class={`rounded-3xl w-full h-full ${bgClass}`}>
+        <div class="backdrop-blur-2xl rounded-3xl">
+            {@render children()}
+        </div>
     </div>
 {/key}
 
